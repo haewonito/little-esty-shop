b68321f49db1441663f6db35f230f6263329c091
Haewon Jeon <haewonito@gmail.com>
2021-11-15 20:38:17 -0700

    fixed api tests

--- spec/features/api/nager_facade_spec.rb
+++ spec/features/api/nager_facade_spec.rb
@@ -1,15 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe NagerFacade do
+RSpec.describe HolidayFacade do
   it 'constructs nagerholiday objects' do
-    mock_response = '[
-          {"date":"2021-11-25","localName":"Thanksgiving Day","name":"Thanksgiving Day","countryCode":"US","fixed":false,"global":true,"counties":null,"launchYear":1863,"types":["Public"]},
-          {"date":"2021-12-24","localName":"Christmas Day","name":"Christmas Day","countryCode":"US","fixed":false,"global":true,"counties":null,"launchYear":null,"types":["Public"]},
-          {"date":"2022-01-17","localName":"Martin Luther King, Jr. Day","name":"Martin Luther King, Jr. Day","countryCode":"US","fixed":false,"global":true,"counties":null,"launchYear":null,"types":["Public"]}
-          ]'
-
-    facade = NagerFacade.new(mock_response)
+    facade = HolidayFacade.holidays
     expect(facade.first).to be_a NagerHoliday
-    expect(GithubFacade.first.name).to eq("Thanksgiving")
+    expect(facade.first.name).to eq("Thanksgiving Day")
   end
 end
+
+#second expectation will have to change when time passes.
+#didn't know how to make dynamic
